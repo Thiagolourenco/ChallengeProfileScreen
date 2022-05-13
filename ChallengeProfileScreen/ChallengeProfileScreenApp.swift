3a8630126aa6522a8e33e0ae6d4e702761afec6e
Thiago Lourenço <thiagolourencosaraiva123@gmail.com>
2022-05-12 22:23:37 -0300

    Initial Commit

---
+++ ChallengeProfileScreen/ChallengeProfileScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeProfileScreenApp.swift
+//  ChallengeProfileScreen
+//
+//  Created by Thiago Lourenço on 12/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeProfileScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
