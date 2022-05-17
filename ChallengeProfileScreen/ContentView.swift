c693fad28de5c442c4459259ac3f98a6700b165a
Thiago Lourenço <thiagolourencosaraiva123@gmail.com>
2022-05-16 22:20:39 -0300

    feat: study swift

--- ChallengeProfileScreen/ContentView.swift
+++ ChallengeProfileScreen/ContentView.swift
@@ -9,8 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            Rectangle().fill(.black).ignoresSafeArea()
+
+            VStack {
+                Image("Header")
+                    .resizable()
+                    .frame(width: .infinity, height: 150)
+                    .ignoresSafeArea()
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 100, trailing: 0))
+                Image("Profile")
+                    .resizable()
+                    .clipShape(Circle())
+                    .frame(width: 200, height: 150)
+                    .padding(EdgeInsets(top: -200, leading: 0, bottom: 0, trailing: 0))
+
+                Text("Assistidos")
+                    .foregroundColor(.white)
+                    .font(.system(size: 24))
+                    .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 240))
+                HStack {
+                    Image("Movie")
+                        .resizable()
+                        .frame(width: 110, height: 200)
+                        .cornerRadius(14)
+                    Image("Movie")
+                        .resizable()
+                        .frame(width: 110, height: 200)
+                        .cornerRadius(14)
+                    Image("Movie")
+                        .resizable()
+                        .frame(width: 110, height: 200)
+                        .cornerRadius(14)
+                }
+                
+                Text("Na Fila")
+                    .foregroundColor(.white)
+                    .font(.system(size: 24))
+                    .multilineTextAlignment(.leading)
+                    .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 270))
+
+                HStack {
+                    Image("Movie")
+                        .resizable()
+                        .frame(width: 110, height: 200)
+                        .cornerRadius(14)
+                    Image("Movie")
+                        .resizable()
+                        .frame(width: 110, height: 200)
+                        .cornerRadius(14)
+                    Image("Movie")
+                        .resizable()
+                        .frame(width: 110, height: 200)
+                        .cornerRadius(14)
+                }
+
+            }
+        }
+       
     }
 }
 
